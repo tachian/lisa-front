ebb85051902f458000572406f0ecaa738c461592
Andre Tachian <andre.tachian.ext@nextel.com.br>
2019-01-30 16:31:38 -0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { HeaderComponent } from './header/header.component';
+import { ApiComponent } from './api/api.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    HeaderComponent,
+    ApiComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule
